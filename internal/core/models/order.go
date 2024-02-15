c7db165ba09b39972f103273e81f025ffa04bf97
MikeMwita <mwitamike8@gmail.com>
2024-02-16 00:27:33 +0300

    Define order model

--- internal/core/models/order.go
+++ internal/core/models/order.go
@@ -1,1 +1,9 @@
 package models
+
+type Order struct {
+	ID         int     `json:"id"`
+	CustomerID int     `json:"customer_id"`
+	Item       string  `json:"item"`
+	Amount     float64 `json:"amount"`
+	Time       string  `json:"time"`
+}
