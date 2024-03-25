dfa24b5d181e6518981f3a3aa035782729f18567
MikeMwita <mwitamike8@gmail.com>
2024-03-25 14:29:44 +0300

    Add logout handler

--- internal/routes/handlers/logout_handler.go
+++ internal/routes/handlers/logout_handler.go
@@ -1,1 +1,41 @@
 package handlers
+
+import (
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	_, span := otel.Tracer("auth-service").Start(r.Context(), "LogoutHandler")
+	defer span.End()
+
+	logoutURL, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/v2/logout")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		span.RecordError(err)
+
+		return
+	}
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+
+	returnTo, err := url.Parse(scheme + "://" + r.Host)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		span.RecordError(err)
+		return
+	}
+
+	parameters := url.Values{}
+	parameters.Add("returnTo", returnTo.String())
+	parameters.Add("client_id", os.Getenv("AUTH0_CLIENT_ID"))
+	logoutURL.RawQuery = parameters.Encode()
+
+	span.SetAttributes(attribute.String("logout.redirect", logoutURL.String()))
+	http.Redirect(w, r, logoutURL.String(), http.StatusTemporaryRedirect)
+}
