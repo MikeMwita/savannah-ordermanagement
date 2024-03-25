6e94d9f6bd3c8f747c05591d5b8768ee1df59bb7
MikeMwita <mwitamike8@gmail.com>
2024-03-25 14:33:14 +0300

    Add isauth middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,27 @@
 package middleware
+
+import (
+	"github.com/gorilla/sessions"
+	"net/http"
+)
+
+var store *sessions.CookieStore
+
+// Checks if user is previously authenticated
+
+func IsAuthenticated(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//
+		session, err := store.Get(r, "session-name")
+		if err != nil {
+			http.Error(w, "Session retrieval failed.", http.StatusInternalServerError)
+			return
+		}
+
+		if profile, ok := session.Values["profile"]; profile == nil || !ok {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	}
+}
