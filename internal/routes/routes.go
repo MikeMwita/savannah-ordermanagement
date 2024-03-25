1476ddbf26852e23321eddc03c361b1d40d85f59
MikeMwita <mwitamike8@gmail.com>
2024-03-25 14:33:44 +0300

    Refactor routes to incude traces

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,26 +1,31 @@
 package routes
 
 import (
+	"fmt"
 	"github.com/MikeMwita/savannah-ordermanagement/internal/core/adapters"
 	"github.com/MikeMwita/savannah-ordermanagement/internal/routes/handlers"
+	"github.com/MikeMwita/savannah-ordermanagement/pkg/authenticator"
+	"github.com/gorilla/sessions"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"net/http"
 )
 
-// RegisterRoutes registers all routes for the application
-func RegisterRoutes(mux *http.ServeMux, orderRepo adapters.OrderRepository, customerRepo adapters.CustomerRepository) {
+// RegisterRoutes --->registers all routes for the application
+func RegisterRoutes(mux *http.ServeMux, orderRepo adapters.OrderRepository, customerRepo adapters.CustomerRepository, auth *authenticator.Authenticator) {
 	handler := handlers.NewHandler(orderRepo, customerRepo)
 
-	// Authentication routes
-	mux.HandleFunc("/auth/google/login", handlers.LoginHandler)
-	mux.HandleFunc("/auth/google/logout", handlers.LogoutHandler)
-	mux.HandleFunc("/auth/google/callback", handlers.CallbackHandler)
-	mux.HandleFunc("/user", handlers.UserInfoHandler)
+	store := sessions.NewCookieStore([]byte("mike"))
+	mux.Handle("/", otelhttp.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Welcome to the Savannah OrderManagement API!")
+	}), "Index"))
 
-	mux.HandleFunc("/signup", handler.AddCustomerHandler)
-	mux.HandleFunc("/order", handler.AddOrderHandler)
+	mux.Handle("/login", otelhttp.NewHandler(handlers.LoginHandler(auth, store), "Login"))
+	mux.Handle("/logout", otelhttp.NewHandler(http.HandlerFunc(handlers.LogoutHandler), "Logout"))
+	mux.Handle("/callback", otelhttp.NewHandler(http.HandlerFunc(handlers.CallBackHandler(auth, store)), "Callback"))
+	mux.Handle("/user", otelhttp.NewHandler(http.HandlerFunc(handlers.UserHandler), "User"))
 
-	// Protected routes
-	//mux.Handle("/signup", middleware.AuthMiddleware(http.HandlerFunc(handler.AddCustomerHandler)))
-	//mux.Handle("/order", middleware.AuthMiddleware(http.HandlerFunc(handler.AddOrderHandler)))
+	//mux.HandleFunc("/user", middleware.IsAuthenticated(user.Handler))
+	mux.Handle("/signup", otelhttp.NewHandler(http.HandlerFunc(handler.AddCustomerHandler), "AddCustomer"))
+	mux.Handle("/order", otelhttp.NewHandler(http.HandlerFunc(handler.AddOrderHandler), "AddOrder"))
 
 }
