cbabf66c790b679bf9c4c674b91802a25c9321e1
MikeMwita <mwitamike8@gmail.com>
2024-03-25 14:19:56 +0300

    Update CI

--- internal/core/usecase/send_sms_alert_test.go
+++ internal/core/usecase/send_sms_alert_test.go
@@ -1,41 +1,24 @@
 package Usecase
 
 import (
-	"testing"
-
 	"github.com/MikeMwita/savannah-ordermanagement/internal/core/models"
+	"testing"
 )
 
 func TestSendSmsAlert(t *testing.T) {
-	// Create a sample order
-	order := models.Order{
-		CustomerID: 1,
-		Item:       "Test Item",
+	type args struct {
+		order models.Order
 	}
-
-	// Define test cases
 	tests := []struct {
 		name    string
-		order   models.Order
+		args    args
 		wantErr bool
 	}{
-		{
-			name:    "Valid order",
-			order:   order,
-			wantErr: false,
-		},
-		// Add more test cases if needed
+		// TODO: Add test cases.
 	}
-
-	// Iterate through test cases
 	for _, tt := range tests {
-		// Run subtest for each test case
 		t.Run(tt.name, func(t *testing.T) {
-			// Call the function under test
-			err := SendSmsAlert(tt.order)
-
-			// Check if the error matches the expectation
-			if (err != nil) != tt.wantErr {
+			if err := SendSmsAlert(tt.args.order); (err != nil) != tt.wantErr {
 				t.Errorf("SendSmsAlert() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
---
+++ pkg/authenticator/auth_test.go
@@ -0,0 +1,66 @@
+package authenticator
+
+import (
+	"context"
+	"golang.org/x/oauth2"
+	"reflect"
+	"testing"
+)
+
+func TestAuthenticator_VerifyIDToken(t *testing.T) {
+	type fields struct {
+		Provider *oidc.Provider
+		Config   oauth2.Config
+	}
+	type args struct {
+		ctx   context.Context
+		token *oauth2.Token
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *oidc.IDToken
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Authenticator{
+				Provider: tt.fields.Provider,
+				Config:   tt.fields.Config,
+			}
+			got, err := a.VerifyIDToken(tt.args.ctx, tt.args.token)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("VerifyIDToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("VerifyIDToken() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    *Authenticator
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := New()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
