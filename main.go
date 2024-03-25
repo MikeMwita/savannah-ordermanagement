3519e9f8f8c45e42a820a4fdbac7f80bfd2bdb8d
MikeMwita <mwitamike8@gmail.com>
2024-03-25 14:35:18 +0300

    Add tracing

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"context"
+	"encoding/gob"
 	"github.com/MikeMwita/savannah-ordermanagement/config"
 	"github.com/MikeMwita/savannah-ordermanagement/internal/core/repository"
 	"github.com/MikeMwita/savannah-ordermanagement/internal/routes"
 	"github.com/MikeMwita/savannah-ordermanagement/pkg"
+	"github.com/MikeMwita/savannah-ordermanagement/pkg/authenticator"
 	"github.com/MikeMwita/savannah-ordermanagement/pkg/utils"
 	"github.com/joho/godotenv"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+const (
+	service     = "savannahotel"
+	environment = "development"
+	id          = 1
+)
+
+func init() {
+	gob.Register(map[string]interface{}{})
+}
+
 func main() {
+	// Initialize OpenTelemetry
+	tp, err := tracerProvider("http://localhost:14269/api/traces")
+	if err != nil {
+		log.Fatalf("Failed to initialize tracer provider: %v", err)
+	}
+	defer func() {
+		// Shutdown the tracer provider to flush any remaining spans
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatalf("Failed to shutdown tracer provider: %v", err)
+		}
+	}()
+	otel.SetTracerProvider(tp)
+
 	log.Println("Starting api server")
 
 	configPath := utils.GetConfigPath(os.Getenv("config"))
@@ -26,10 +61,10 @@ func main() {
 		log.Fatalf("ParseConfig: %v", err)
 	}
 
-	err = godotenv.Load(".env")
+	err = godotenv.Load(".env_example")
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env_example file")
 	}
 
 	pkg.InitDB()
@@ -37,16 +72,34 @@ func main() {
 	orderRepo := repository.NewOrderRepo(pkg.DB)
 	customerRepo := repository.NewCustomerRepo(pkg.DB)
 
-	// Create a new ServeMux
-	mux := http.NewServeMux()
-
-	// Register authentication and protected routes
-	routes.RegisterRoutes(mux, orderRepo, customerRepo)
-
-	port := ":5557"
-
-	log.Printf("Server is running on port %s...\n", port)
-	if err := http.ListenAndServe(port, mux); err != nil {
-		log.Fatalf("Failed to start server: %v\n", err)
+	auth, err := authenticator.New()
+	if err != nil {
+		log.Fatalf("Failed to initialize the authenticator: %v", err)
 	}
+
+	rtr := http.NewServeMux()
+	routes.RegisterRoutes(rtr, orderRepo, customerRepo, auth)
+
+	log.Print("Server listening on http://localhost:3001/")
+	if err := http.ListenAndServe("0.0.0.0:3001", rtr); err != nil {
+		log.Fatalf("There was an error with the http server: %v", err)
+	}
+
+}
+
+func tracerProvider(url string) (*trace.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(service),
+			attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
 }
